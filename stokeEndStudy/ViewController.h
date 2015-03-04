340eaa87757463b7d27c724812ddb3cd7142c2ac
Various <apple123@mail.ustc.edu.cn>
2015-03-04 11:26:48 +0800

    init

---
+++ stokeEndStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stokeEndStudy
+//
+//  Created by Tim Geng on 3/3/15.
+//  Copyright (c) 2015 PwC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
